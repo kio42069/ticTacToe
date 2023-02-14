a063b28cabad18e3774487b9a9566a583f462460
Surat Sathi Samanta <62372847+surat-ss@users.noreply.github.com>
2023-02-14 18:04:18 +0530

    test

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//checks to see if a player has won or if game is draw
+int checkForWin();
+
+//redraws the board for each turn
+void drawBoard();
+
+//sets the char array with a selection and check for an invalid selection
+void markBoard();
+
+int main()
+{
+    return 0;
+}
+
+int checkForWin(char arr[9])
+{
+    if(arr)
+}
